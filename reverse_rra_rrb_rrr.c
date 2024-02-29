0353be7f09fcb9a7e732dcff82c9fad09cb78b1b
Alexis Ottolini <alexisottolini@MacBook-Pro-de-Alexis.local>
2024-02-29 15:53:57 +0100

    swap-rotate-reverse-push

--- reverse_rra_rrb_rrr.c
+++ reverse_rra_rrb_rrr.c
@@ -1,3 +1,39 @@
 #include "libpushswap.h"
 
+static void	reverse_rotate(t_list **stack)
+{
+	t_list	*lastnode;
+	int	len;
 
+	len = stack_len(*stack);
+	if (stack == NULL || *stack == NULL || len == 1)
+		return ;
+	lastnode = find_lastnode(*stack);
+	lastnode->prev->next = NULL;
+	lastnode->next = *stack;
+	*stack = lastnode;
+	lastnode->prev = NULL;
+	lastnode->prev->next = lastnode;
+}
+
+void	rra(t_list **a, bool checker)
+{
+	reverse_rotate(a);
+	if (!checker)
+		write(1, "rra\n", 4); 
+}
+
+void	rrb(t_list **b, bool checker)
+{
+	reverse_rotate(b);
+	if (!checker)
+		write(1, "rrb\n", 4);
+}
+
+void	rrr(t_list **a, t_list **b, bool checker)
+{
+	reverse_rotate(a);
+	reverse_rotate(b);
+	if (!checker)
+		write(1, "rrr\n", 4);
+}
--- swap_sa_sb_ss.c
+++ swap_sa_sb_ss.c
@@ -1,3 +1,41 @@
 #include "libpushswap.h"
 
+static void	swap(t_list **stack)
+{
+	t_list	*swap;
+	int	len;
 
+	len = stack_len(*stack);
+	if (*stack == NULL || stack == NULL || len == 1)
+		return ;
+	swap = *stack;
+    *stack = (*stack)->next;
+    (*stack)->prev = NULL;
+    (*stack)->next->prev = swap;
+    swap->next = (*stack)->next;
+    (*stack)->next = swap;
+    swap->prev = *stack;	
+
+}
+
+void	sa(t_list **a, bool checker)
+{
+	swap(a);
+	if (!checker)
+		write(1, "sa\n", 3);
+}
+
+void	sb(t_list **b, bool checker)
+{
+	swap(b);
+	if (!checker)
+		write(1, "sb\n", 3);
+}
+
+void	ss(t_list **a, t_list **b, bool checker)
+{
+	swap(a);
+	swap(b);
+	if (!checker)
+		write(1, "ss\n", 3);
+}
